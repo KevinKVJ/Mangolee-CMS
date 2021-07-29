31758d64df7a2e57ead8eaefaaafd836c449da34
miaoxudong111 <xudong.miao1@ucalgary.ca>
2021-07-29 17:39:53 -0600

    Update GlobalExceptionHandler.java

--- MangoLee-CMS-common/src/main/java/org/mangolee/utils/GlobalExceptionHandler.java
+++ MangoLee-CMS-common/src/main/java/org/mangolee/utils/GlobalExceptionHandler.java
@@ -12,15 +12,16 @@ public class GlobalExceptionHandler<T> {
     // 处理业务异常
     @ExceptionHandler({BaseException.class})
     public Result<T> baseExceptionHandler(BaseException e) {
-        return ResultUtils.error(e.getCode(),e.getMessage());
+        return Result.error(e.getCode(),e.getMessage());
     }
 
     // 处理其他异常
     @ExceptionHandler({Exception.class})
     public Result<T> exceptionHandler(Exception e) {
-        return ResultUtils.error(ResultEnum.BAD_REQUEST);
+        return Result.BAD_REQUEST;
     }
 
+    //处理远程调用异常
     @ExceptionHandler({MyFeignException.class})
     public Result FeignExceptionHandler(Exception e){
         return Result.INTERNAL_ERROR;
