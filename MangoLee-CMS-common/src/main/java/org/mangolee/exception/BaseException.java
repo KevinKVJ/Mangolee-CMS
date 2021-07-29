aa926ee2c15f8c1716ee53c652b764dedfe31bd5
miaoxudong111 <xudong.miao1@ucalgary.ca>
2021-07-29 17:43:15 -0600

    Update BaseException.java

--- MangoLee-CMS-common/src/main/java/org/mangolee/exception/BaseException.java
+++ MangoLee-CMS-common/src/main/java/org/mangolee/exception/BaseException.java
@@ -3,6 +3,7 @@ package org.mangolee.exception;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
+import org.mangolee.utils.Result;
 
 
 @ApiModel("自定义基础异常类")
@@ -12,8 +13,8 @@ public class BaseException extends RuntimeException {
 
     private Integer code;
 
-    public BaseException(ResultEnum resultEnum) {
-        this(resultEnum.getCode(), resultEnum.getMessage());
+    public BaseException(Result result) {
+        this(result.getCode(), result.getMessage());
     }
 
     private BaseException(Integer code, String message) {
