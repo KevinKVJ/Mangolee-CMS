b0d758a6d31b65b1bca84c4d2a7733dfc0ce8dc4
zpeng11 <zhanfei.peng@ucalgary.ca>
2021-08-06 19:26:14 -0600

    将gateway从common中脱钩，方便编写interceptor“

--- MangoLee-CMS-Gateway/src/main/java/org/mangolee/GatewayApp.java
+++ MangoLee-CMS-Gateway/src/main/java/org/mangolee/GatewayApp.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {org.mangolee.utils.GlobalExceptionHandler.class}))
 @EnableDiscoveryClient
 public class GatewayApp {
     public static void main(String[] args) {
---
+++ MangoLee-CMS-Gateway/src/main/java/org/mangolee/entity/UserInfo.java
@@ -0,0 +1,39 @@
+package org.mangolee.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value = "用户信息类")
+public class UserInfo {
+    // 主键ID
+    @ApiModelProperty(value = "主键ID", example = "1")
+    private Long    id;
+
+    // 用户名
+    @ApiModelProperty(value = "用户名", example = "Jack")
+    private String  username;
+
+    // 邮箱
+    @ApiModelProperty(value = "邮箱", example = "user@mangolee.com")
+    private String email;
+
+    // 角色权限
+    @ApiModelProperty(value = "角色权限", example = "ADMIN")
+    private String role;
+
+    // UUID (保证token随机性)
+    @ApiModelProperty(value = "UUID")
+    private String uuid;
+
+    // 创建时间
+    @ApiModelProperty(value = "创建时间")
+    private Date gmtCreate;
+}
