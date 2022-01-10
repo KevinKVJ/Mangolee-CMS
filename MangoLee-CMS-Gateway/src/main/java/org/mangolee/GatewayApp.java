11c3e3051af109a621d649185ba8e4bdcc704c3c
zpeng11 <zhanfei.peng@ucalgary.ca>
2022-01-10 15:13:31 -0600

    gateway使用token验证

--- MangoLee-CMS-Gateway/src/main/java/org/mangolee/GatewayApp.java
+++ MangoLee-CMS-Gateway/src/main/java/org/mangolee/GatewayApp.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"org.mangolee.service"})
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class,args);
