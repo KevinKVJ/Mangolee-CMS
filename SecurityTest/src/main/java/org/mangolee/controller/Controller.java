4bbbe530c0af035da91e164acbc81d63921afcc6
zpeng11 <zhanfei.peng@ucalgary.ca>
2022-01-12 20:28:35 -0600

    Add log configuration to common-log and finished testing

--- SecurityTest/src/main/java/org/mangolee/controller/Controller.java
+++ SecurityTest/src/main/java/org/mangolee/controller/Controller.java
@@ -1,6 +1,9 @@
 package org.mangolee.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.mangolee.Application;
 import org.mangolee.entity.Result;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
+    private Logger logger = LoggerFactory.getLogger(Application.class);
     @GetMapping("/test")
     public Result<String> test(@RequestHeader MultiValueMap<String, String> headers)
     {
@@ -20,5 +24,12 @@ public class Controller {
                     "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
         });
         return Result.success("Header info： "+headers.toString());
+    }
+
+    @GetMapping("/test2")
+    public Result<String> test2()
+    {
+        logger.error("Hello logger");
+        return Result.success("Success");
     }
 }
